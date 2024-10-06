db0b6d3191ddbf4ca7064b3dd733b04e225d780a
Prajwal <sharmaprajwal11@gmail.com>
2024-10-07 00:15:19 +0530

    learn: how to handle models

--- learnDjango/learnDjango/urls.py
+++ learnDjango/learnDjango/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,6 +26,7 @@ urlpatterns = [
     path('about/',views.about, name='about'),
     path('contact/',views.contact, name='contact'),
     path('learningJinja/',include('learningJinja.urls')),
-    path ("_reload_/", include("django_browser_reload.urls")),
+    path ("__reload__/", include("django_browser_reload.urls")),
 
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
