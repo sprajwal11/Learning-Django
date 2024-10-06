0b9d46452d9807d64b19d20757000a4b2888ba79
Prajwal <sharmaprajwal11@gmail.com>
2024-10-06 20:13:27 +0530

    tailwind in Django and super user

--- learnDjango/learnDjango/urls.py
+++ learnDjango/learnDjango/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('about/',views.about, name='about'),
     path('contact/',views.contact, name='contact'),
     path('learningJinja/',include('learningJinja.urls')),
+    path ("_reload_/", include("django_browser_reload.urls")),
 
 ]
