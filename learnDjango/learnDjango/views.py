fdb65992aa0d4d3ecc15719efa79d3c33f6607c2
Prajwal <sharmaprajwal11@gmail.com>
2024-10-06 11:02:21 +0530

    feat: Implimented Teplates

--- learnDjango/learnDjango/views.py
+++ learnDjango/learnDjango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello world!")
+    # return HttpResponse("<h1>Hello world!</h1>")
+    return render(request,'website/index.html')
 
 def about(request):
     return HttpResponse("This is about page")
